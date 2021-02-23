cbbf58638a1d5b2d978e6abeb8731e8b3fd952d0
Peter <dhondt.peter@gmail.com>
2021-02-23 15:51:14 +0100

    step 9 create Player methods

--- Player.php
+++ Player.php
@@ -5,32 +5,40 @@ require 'Deck.php';
 
 class Player
 {
-    private array $cards;
+    private const MAX_SCORE = 21;
+    private array $cards = [];
     private bool $lost = false;
+    //private int $score;
 
     public function __construct(Deck $deck)
     {
         array_push($this->cards, $deck->drawCard(), $deck->drawCard());
     }
 
-    public function hit()
+    public function hit(Deck $deck): void
     {
-
+        array_push($this->cards, $deck->drawCard());
+        if ($this->getScore() > self::MAX_SCORE) {
+            $this->lost = true;
+        }
     }
 
-    public function surrender()
+    public function surrender(): void
     {
-
+        $this->lost = true;
     }
 
-    public function getScore()
+    public function getScore(): int
     {
-
+        foreach($this->cards AS $card) {
+            $totalscore += $card->getValue();
+        }
+        return $totalscore;
     }
 
-    public function hasLost()
+    public function hasLost(): bool
     {
-
+        return $this->lost;
     }
 
 }
